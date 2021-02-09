30f8c3ffcabd29352df6b1d4448448b6326076f2
Elias Fang <eliasfang@gmail.com>
2021-02-09 11:47:53 -0800

    Implement map and tap hold

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,14 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'gwqQxGuVnX1uzjlAJSjIo79zpetAjFYT';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+	  center: [32.878737, -117.235832],
+	  layers: L.mapquest.tileLayer('map'),
+	  zoom: 12,
+	  zoomControl: false
+	});
+
+	// Add a marker to the map
+	L.marker([32.878737, -117.235832]).addTo(map);
 }
